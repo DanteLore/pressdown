b871cbfd1c98f634a5c2bb1912e9d55a2e0bc504
Dan Taylor <Dan.Taylor@thetrainline.com>
2020-03-16 22:59:52 +0000

    Code formatting working pretty well, but a few tweaks to make it more markdown friendly

--- convert.py
+++ convert.py
@@ -15,11 +15,15 @@ items = soup.find_all('item')
 
 
 def convert_content(c):
+    c = c.replace('&nbsp;', ' ')
     c = re.sub(r'<!-- /?wp:paragraph -->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<!-- /?wp:heading.*?-->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<!-- /?wp:image.*?-->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<!-- /?wp:code.*?-->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<!-- /?wp:gallery.*?-->', '', c, flags=re.MULTILINE)
+    c = re.sub(r'<!-- /?wp:list.*?-->', '', c, flags=re.MULTILINE)
+    c = re.sub(r'<!-- /?wp:separator.*?-->', '', c, flags=re.MULTILINE)
+    c = re.sub(r'<!-- /?wp:quote.*?-->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<h1>(.*?)</h1>', r'# \g<1>', c, flags=re.MULTILINE)
     c = re.sub(r'<h2>(.*?)</h2>', r'## \g<1>', c, flags=re.MULTILINE)
     c = re.sub(r'<h3>(.*?)</h3>', r'### \g<1>', c, flags=re.MULTILINE)
@@ -30,14 +34,26 @@ def convert_content(c):
     c = re.sub(r'<!-- wp:core-embed/youtube.*?-->[\S\s]*?https://youtu.be/([^\s]*)[\S\s]*?<!-- /wp:core-embed/youtube -->',
                r'\n{{< youtube \g<1> >}}\n', c, flags=re.MULTILINE)
     c = re.sub(r'<figcaption>.*?</figcaption>', '', c, flags=re.MULTILINE)
-    c = re.sub(r'<ul.*?wp-block-gallery.*?>(.*?)</ul>', '\g<1>', c, flags=re.MULTILINE)
+    c = re.sub(r'<ul.*?>(.*?)</ul>', '\g<1>', c, flags=re.MULTILINE)
+    c = re.sub(r'<li.*?>(.*?)</li>', '* \g<1>\n', c, flags=re.MULTILINE)
     c = re.sub(r'<img.*?src="([^\"]*)".*?/>', '<img src="\g<1>"/>', c, flags=re.MULTILINE)
-    c = re.sub(r'<li class="blocks-gallery-item">.*?<img.*?src="([^\"]*)".*?/>.*?</li>', r'<img src="\g<1>" class="gallery"/>', c, flags=re.MULTILINE)
+    c = re.sub(r'<li class="blocks-gallery-item">.*?<img.*?src="([^\"]*)".*?/>.*?</li>',
+               r'<img src="\g<1>" class="gallery"/>', c, flags=re.MULTILINE)
     c = re.sub(r'<em>(.*?)</em>', '*\g<1>*', c, flags=re.MULTILINE)
     c = re.sub(r'<del>(.*?)</del>', '~~\g<1>~~', c, flags=re.MULTILINE)
+    c = re.sub(r'<hr.*?>', '\n---\n', c, flags=re.MULTILINE)
     c = re.sub(r'<a.*href="(.*?)".*?>(.*?)</a>', r'<a href="\g<1>">\g<2></a>', c, flags=re.MULTILINE)
-    c = re.sub(r'<code.*?>([\S\s]*?)</code>', r'```\n\g<1>\n```', c, flags=re.MULTILINE)
-    c = re.sub(r'<br.*?/>', '\n', c, flags=re.MULTILINE)
+    c = re.sub(r'<code.*?>([\S\s]*?)</code>', r'```\g<1>```', c, flags=re.MULTILINE)
+    c = re.sub(r'<br.*?>', '\n', c, flags=re.MULTILINE)
+    c = re.sub(r'<!-- wp:syntaxhighlighter/code {"language":"(.*?)"} -->([\S\s]*?)<!-- /wp:syntaxhighlighter/code -->',
+               '\n{{< highlight \g<1> >}} \g<2> {{< / highlight >}}\n', c, flags=re.MULTILINE)
+    c = re.sub(r'\[sourcecode language="(.*?)"\]([\S\s]*?)\[/sourcecode\]',
+               '\n{{< highlight \g<1> >}} \g<2> {{< / highlight >}}\n', c, flags=re.MULTILINE)
+    c = re.sub(r'<!-- wp:syntaxhighlighter/code -->([\S\s]*?)<!-- /wp:syntaxhighlighter/code -->',
+               '```\g<1>```', c, flags=re.MULTILINE)
+    c = re.sub(r'<!-- wp:preformatted -->([\S\s]*?)<!-- /wp:preformatted -->',
+               '```\g<1>```', c, flags=re.MULTILINE)
+    c = re.sub(r'{{< highlight jscript >}}', r'{{< highlight javascript >}}', c)
     c = re.sub(r'\n\n\n', r'\n', c, flags=re.MULTILINE)
     return c
 
