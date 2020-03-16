ce981948752e31372cfb99fb31a6b6d84d5cb1a4
Dan Taylor <Dan.Taylor@thetrainline.com>
2020-03-16 20:33:11 +0000

    Processing images nicely with a class

--- convert.py
+++ convert.py
@@ -20,22 +20,23 @@ def convert_content(c):
     c = re.sub(r'<!-- /?wp:image.*?-->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<!-- /?wp:code.*?-->', '', c, flags=re.MULTILINE)
     c = re.sub(r'<!-- /?wp:gallery.*?-->', '', c, flags=re.MULTILINE)
-    c = re.sub(r'<h1>(.*?)</h1>', r'#\g<1>', c, flags=re.MULTILINE)
-    c = re.sub(r'<h2>(.*?)</h2>', r'##\g<1>', c, flags=re.MULTILINE)
-    c = re.sub(r'<h3>(.*?)</h3>', r'###\g<1>', c, flags=re.MULTILINE)
-    c = re.sub(r'<h4>(.*?)</h4>', r'####\g<1>', c, flags=re.MULTILINE)
+    c = re.sub(r'<h1>(.*?)</h1>', r'# \g<1>', c, flags=re.MULTILINE)
+    c = re.sub(r'<h2>(.*?)</h2>', r'## \g<1>', c, flags=re.MULTILINE)
+    c = re.sub(r'<h3>(.*?)</h3>', r'### \g<1>', c, flags=re.MULTILINE)
+    c = re.sub(r'<h4>(.*?)</h4>', r'#### \g<1>', c, flags=re.MULTILINE)
     c = re.sub(r'</?p.*?>', '', c, flags=re.MULTILINE)
     c = re.sub(r'</?div.*?>', '', c, flags=re.MULTILINE)
     c = re.sub(r'</?figure.*?>', '', c, flags=re.MULTILINE)
     c = re.sub(r'<figcaption>.*?</figcaption>', '', c, flags=re.MULTILINE)
     c = re.sub(r'<ul.*?wp-block-gallery.*?>(.*?)</ul>', '\g<1>', c, flags=re.MULTILINE)
-    c = re.sub(r'<li class="blocks-gallery-item">.*?<img.*?src="([^\"]*)".*?/>.*?</li>', '![](\g<1>)\n', c, flags=re.MULTILINE)
-    c = re.sub(r'<img.*?src="([^\"]*)".*?/>', '![](\g<1>)', c, flags=re.MULTILINE)
+    c = re.sub(r'<img.*?src="([^\"]*)".*?/>', '<img src="\g<1>"/>', c, flags=re.MULTILINE)
+    c = re.sub(r'<li class="blocks-gallery-item">.*?<img.*?src="([^\"]*)".*?/>.*?</li>', '<img src="\g<1>" class="gallery"/>', c, flags=re.MULTILINE)
     c = re.sub(r'<em>(.*?)</em>', '*\g<1>*', c, flags=re.MULTILINE)
     c = re.sub(r'<del>(.*?)</del>', '~~\g<1>~~', c, flags=re.MULTILINE)
     c = re.sub(r'<a.*href="([^\"]*)".*?>(.*?)</a>', '[\g<2>](\g<1>)', c, flags=re.MULTILINE)
     c = re.sub(r'<code.*?>([\S\s]*?)</code>', '```\n\g<1>\n```', c, flags=re.MULTILINE)
-    c = re.sub(r'\n\n', r'\n', c, flags=re.MULTILINE)
+    c = re.sub(r'<br.*?/>', '\n', c, flags=re.MULTILINE)
+    c = re.sub(r'\n\n\n', r'\n', c, flags=re.MULTILINE)
     return c
 
 
@@ -48,7 +49,7 @@ def write_post(item):
     print("\n\nCONVERTING POST: {0}\n\nFilename: {1}\nPost Date: {2}\nStatus: {3}".format(title, filename, published, status))
 
     with open(filename, 'w') as f:
-        f.write("\n---\ntitle: {0}\ndate: {1}\ndraft: {2}\n---\n\n".format(title, published, status != 'publish'))
+        f.write('\n---\ntitle: "{0}"\ndate: {1}\ndraft: {2}\n---\n\n'.format(title, published, status != 'publish'))
         f.write(content)
 
 
